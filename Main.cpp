a49247852f2431fd655008cefad98aed8d4efaa7
silvino00 <47743896+silvino00@users.noreply.github.com>
2019-03-11 13:59:11 +0100

    Commit 9

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,14 @@
-
 #include "SDL/include/SDL.h"
 #include <iostream>
 #include <time.h>
 #include "SDL_Image/include/SDL_image.h"
+#include "SDL_Mixer/include/SDL_mixer.h"
 
 #pragma comment(lib, "SDL/libx86/SDL2.lib")
 #pragma comment(lib, "SDL/libx86/SDL2main.lib")
 #pragma comment(lib, "SDL_Image/libx86/SDL2_image.lib")
+#pragma comment(lib, "SDL_Mixer/libx86/SDL2_mixer.lib")
+
 
 using namespace std;
 
@@ -15,7 +17,16 @@ int main(int argc, char *argv[]) {
 	SDL_Window* window;
 	SDL_Renderer* render;
 	SDL_Surface *background;
+	SDL_Surface *spaceship;
+	SDL_Surface *laser;
+	//utilizamos la funcion para definir un rectangulo
+	SDL_Rect rectangle;
+	//utilizamos la funcion para definir un rectangulo
+	SDL_Rect rectangle2;
+	
 	background = IMG_Load("Assets/background.png");
+	spaceship = IMG_Load("Assets/descarga.png");
+	laser = IMG_Load("Assets/laser.png");
 	//SDL_Surface* surf;
 	//SDL_Texture* background;
 	//SDL_Texture* blueshapes;
@@ -25,6 +36,31 @@ int main(int argc, char *argv[]) {
 	int up = 0, left = 0, right = 0, down = 0;
 
 	int imgflags = IMG_INIT_PNG;
+
+
+	//coordenadas inicializadas para que el quadrado despues se mueva
+	int x = 320;
+	int y = 240;
+
+	int z = x + 25;
+	int w = y + 25;
+
+	//asignamos posicion x e y con semilla aleatoria
+	rectangle.x = x;
+	rectangle.y = y;
+	//asignamos alto y ancho en px
+	rectangle.w = 50;
+	rectangle.h = 50;
+
+	//asignamos posicion x e y con semilla aleatoria
+	rectangle2.x = z;
+	rectangle2.y = w;
+	//asignamos alto y ancho en px
+	rectangle2.w = 40;
+	rectangle2.h = 30;
+
+
+
 	
 
 	//Iniciamos SDL en modo video
@@ -35,15 +71,9 @@ int main(int argc, char *argv[]) {
 	//dibujamos nuestra app
 	render = SDL_CreateRenderer(window, -1, 0);
 	SDL_Texture *background_text = SDL_CreateTextureFromSurface(render, background);
+	SDL_Texture *spaceship_text = SDL_CreateTextureFromSurface(render, spaceship);
+	SDL_Texture *laser_text = SDL_CreateTextureFromSurface(render, laser);
 	
-
-	//coordenadas inicializadas para que el quadrado despues se mueva
-	int x = 320;
-	int y = 240;
-	
-	int z = x + 25;
-	int w = y + 25;
-
 	//añadimos un loop infinito
 	while (num==1) {
 		SDL_Event event;
@@ -55,25 +85,25 @@ int main(int argc, char *argv[]) {
 					num = 0;
 					break;
 				case SDLK_LEFT:
-					left=1;
-					x -= 20;
+					left = 1;
+					rectangle.x -= 1;
 					break;
 				case SDLK_RIGHT:
-					right=1;
-					x += 20;
+					right = 1;
+					rectangle.x += 1;
 					break;
 				case SDLK_UP:
-					up=1;
-					y -= 20;
+					up = 1;
+					rectangle.y -= 1;
 					break;
 				case SDLK_DOWN:
-					down=1;
-					y += 20;
+					down = 1;
+					rectangle.y += 1;
 					break;
 				case SDLK_SPACE:
-					spacekey=1;
-					z = x;
-					w = y + 25;
+					spacekey = 1;
+					rectangle2.x= rectangle.x;
+					rectangle2.y = rectangle.y + 25;
 					break;
 				default:
 					break;
@@ -105,43 +135,44 @@ int main(int argc, char *argv[]) {
 		}
 		if (up == 1) {
 			if (right == 1) {
-				x += 10;
-				y -= 10;
+				rectangle.x += 1;
+				rectangle.y -= 1;
 			}
 			else if (left == 1) {
-				x -= 10;
-				y -= 10;
+				rectangle.x -= 1;
+				rectangle.y -= 1;
 			}
 			else {
-				y -= 10;
+				rectangle.y -= 1;
 			}
 		}
 		else if (down == 1) {
 			if (right == 1) {
-				x += 10;
-				y += 10;
+				rectangle.x += 1;
+				rectangle.y += 1;
 			}
 			else if (left == 1) {
-				x -= 10;
-				y += 10;
+				rectangle.x -= 1;
+				rectangle.y += 1;
 			}
 			else {
-				y += 10;
+				rectangle.y += 1;
 			}
 		}
 		else if (right == 1) {
-			x += 10;
+			rectangle.x += 1;
 		}
-		else if (down == 1) {
-			x -= 10;
+		else if (left == 1) {
+			rectangle.x -= 1;
 		}
-		z = z + 20;
+		rectangle2.x = rectangle2.x + 1;
 	
 	
 		//llenamos la pantalla del color seleccionado anteriormente
 		SDL_RenderClear(render);
 		SDL_RenderCopy(render, background_text, NULL, NULL);
-		
+		SDL_RenderCopy(render, spaceship_text, NULL, &rectangle);
+		SDL_RenderCopy(render, laser_text, NULL, &rectangle2);
 		//actualizamos y mostramos lo que hay en pantalla tras renderizar capa a capa
 		SDL_RenderPresent(render);
 
@@ -149,12 +180,19 @@ int main(int argc, char *argv[]) {
 	}
 	//destruimos la ventana creada anteriormente
 	SDL_DestroyWindow(window);
+	SDL_DestroyTexture(background_text);
+	SDL_DestroyTexture(spaceship_text);
+	SDL_DestroyTexture(laser_text);
 	SDL_FreeSurface(background);
+	SDL_FreeSurface(spaceship);
+	SDL_FreeSurface(laser);
 	IMG_Quit();
 	SDL_Quit();
 
 	return 0;
 	
 }
+
+
 
 
