399527c81079906b89aea050ecf4ef7e08c4a6ef
silvino00 <47743896+silvino00@users.noreply.github.com>
2019-03-11 11:58:02 +0100

    Commit 8

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,4 @@
+
 #include "SDL/include/SDL.h"
 #include <iostream>
 #include <time.h>
@@ -13,21 +14,28 @@ int main(int argc, char *argv[]) {
 	
 	SDL_Window* window;
 	SDL_Renderer* render;
-	SDL_Surface* surf;
-	SDL_Texture* background;
-	SDL_Texture* blueshapes;
+	SDL_Surface *background;
+	background = IMG_Load("Assets/background.png");
+	//SDL_Surface* surf;
+	//SDL_Texture* background;
+	//SDL_Texture* blueshapes;
 
 	int num = 1;
 	int spacekey = 0;
+	int up = 0, left = 0, right = 0, down = 0;
 
-	int flags = IMG_INIT_JPG | IMG_INIT_PNG;
-	int initted = IMG_Init(flags);
+	int imgflags = IMG_INIT_PNG;
+	
 
 	//Iniciamos SDL en modo video
 	int SDL_init(SDL_INIT_VIDEO);
 
 	window = SDL_CreateWindow("Box", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_RESIZABLE);
+	
+	//dibujamos nuestra app
 	render = SDL_CreateRenderer(window, -1, 0);
+	SDL_Texture *background_text = SDL_CreateTextureFromSurface(render, background);
+	
 
 	//coordenadas inicializadas para que el quadrado despues se mueva
 	int x = 320;
@@ -47,18 +55,23 @@ int main(int argc, char *argv[]) {
 					num = 0;
 					break;
 				case SDLK_LEFT:
+					left=1;
 					x -= 20;
 					break;
 				case SDLK_RIGHT:
+					right=1;
 					x += 20;
 					break;
 				case SDLK_UP:
+					up=1;
 					y -= 20;
 					break;
 				case SDLK_DOWN:
+					down=1;
 					y += 20;
 					break;
 				case SDLK_SPACE:
+					spacekey=1;
 					z = x;
 					w = y + 25;
 					break;
@@ -67,71 +80,81 @@ int main(int argc, char *argv[]) {
 				}
 				break;
 			
-			case SDL_QUIT:
-				num = 0;
-				break;
+			case SDL_KEYUP:
+				switch (event.key.keysym.sym) {
+				case SDLK_UP:
+					up = 0;
+					break;
+				case SDLK_DOWN:
+					down = 0;
+					break;
+				case SDLK_RIGHT:
+					right = 0;
+					break;
+				case SDLK_LEFT:
+					left = 0;
+					break;
+				case SDLK_ESCAPE:
+					spacekey = 0;
+					break;
+				default:
+					break;
+				}
 			}
 
-			
 		}
-			
+		if (up == 1) {
+			if (right == 1) {
+				x += 10;
+				y -= 10;
+			}
+			else if (left == 1) {
+				x -= 10;
+				y -= 10;
+			}
+			else {
+				y -= 10;
+			}
+		}
+		else if (down == 1) {
+			if (right == 1) {
+				x += 10;
+				y += 10;
+			}
+			else if (left == 1) {
+				x -= 10;
+				y += 10;
+			}
+			else {
+				y += 10;
+			}
+		}
+		else if (right == 1) {
+			x += 10;
+		}
+		else if (down == 1) {
+			x -= 10;
+		}
 		z = z + 20;
 	
-		//dibujamos nuestra app
-		render = SDL_CreateRenderer(window, -1, 0);
-
-		//seleccionamos el color de fondo en RGB->azul
-		SDL_SetRenderDrawColor(render, 0, 0, 255, 255);
-
+	
 		//llenamos la pantalla del color seleccionado anteriormente
 		SDL_RenderClear(render);
-
-
-		//utilizamos la funcion para definir un rectangulo
-		SDL_Rect rectangle;
-
-		//colocamos el cuadrado rojo
-		SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
-
-		//rellenamos el rectangulo del color que hemos escogido
-		SDL_RenderFillRect(render, &rectangle);
-
+		SDL_RenderCopy(render, background_text, NULL, NULL);
+		
 		//actualizamos y mostramos lo que hay en pantalla tras renderizar capa a capa
 		SDL_RenderPresent(render);
 
-		//utilizamos la funcion para definir un rectangulo
-		SDL_Rect rectangle2;
-
-		//colocamos la bala de la nave de color verde
-		SDL_SetRenderDrawColor(render, 0, 255, 0, 0);
-
-		//rellenamos el rectangulo del color que hemos escogido
-		SDL_RenderFillRect(render, &rectangle2);
-
-		//actualizamos y mostramos lo que hay en pantalla tras renderizar capa a capa
-		SDL_RenderPresent(render);
-
-		//asignamos posicion x e y con semilla aleatoria
-		rectangle.x = x;
-		rectangle.y = y;
-		//asignamos alto y ancho en px
-		rectangle.w = 50;
-		rectangle.h = 50;
-
-		//asignamos posicion x e y con semilla aleatoria
-		rectangle2.x = z;
-		rectangle2.y = w;
-		//asignamos alto y ancho en px
-		rectangle2.w = 10;
-		rectangle2.h = 5;
-
-		//destruimos el render para poder comenzar de nuevo otra posicion aleatoria
-		SDL_DestroyRenderer(render);
+		
 	}
 	//destruimos la ventana creada anteriormente
 	SDL_DestroyWindow(window);
-
+	SDL_FreeSurface(background);
+	IMG_Quit();
 	SDL_Quit();
 
 	return 0;
+	
 }
+
+
